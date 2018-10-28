99a85a3328571c3db721894147ed61a85e3ffd85
linasmnew <linasmnew@gmail.com>
2018-10-28 14:10:07 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            aaaaaaaa
+            wooot
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            eieiieieeie feature branch
           </a>
         </header>
       </div>
